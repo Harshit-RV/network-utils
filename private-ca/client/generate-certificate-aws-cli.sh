96c2037b7e4460fb5edfa2997f8b49a20ceea0c0
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-06-02 15:34:18 +0530

    fix: generate certificate using AWS CLI script
    - Fixed payload structure as required for Lambda invocation using jq.
    - Added support for AWS profile, AWS_STS_REGION and SYSTEM_SSH_DIR as parameters.
    - Updated CA action names
    - Refactoring

--- private-ca/client/generate-certificate-aws-cli.sh
+++ private-ca/client/generate-certificate-aws-cli.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
+AWS_PROFILE=${1:-"default"}
+AWS_STS_REGION=${2:-"ap-southeast-1"}
+SYSTEM_SSH_DIR=${3:-"/etc/ssh"}
 CA_LAMBDA_FUNCTION_NAME="privateCA"
+LAMBDA_REGION=${AWS_REGION:-'us-west-2'}
 
 # Edit values here
 ######################################################
-# CA_ACTION="getHostSSHCert"
-# CA_ACTION="getClientSSHCert"
-CA_ACTION="generateRootX509Cert"
-# CA_ACTION="generateClientX509Cert"
-
-# # Get client SSL certificate
-# SSL_ATTRS_VALIDITY=""
-# SSL_CLIENT_PUBKEY_PEM=""
+CA_ACTION="generateHostSSHCert"
+# CA_ACTION="generateClientSSHCert"
 
 # # Get host SSH certificate
 # SSH_ATTRS_VALIDITY=""
@@ -23,44 +20,54 @@ CA_ACTION="generateRootX509Cert"
 ######################################################
 
 # Temporary Credentials
-TEMP_CREDS=$(aws sts get-session-token)
+TEMP_CREDS=$(aws sts get-session-token --profile $AWS_PROFILE)
 
 ACCESS_KEY_ID=$(echo $TEMP_CREDS | jq -r ".Credentials.AccessKeyId")
 SECRET_ACCESS_KEY=$(echo $TEMP_CREDS | jq -r ".Credentials.SecretAccessKey")
 SESSION_TOKEN=$(echo $TEMP_CREDS | jq -r ".Credentials.SessionToken")
 
 PYTHON_EXEC=$(which python || which python3)
-LAMBDA_REGION=${AWS_REGION:-'ap-south-1'}
 
 # Auth Headers
 $PYTHON_EXEC -m venv env && source env/bin/activate
 pip install boto3
 
-output=$($PYTHON_EXEC auth-header.py $ACCESS_KEY_ID $SECRET_ACCESS_KEY $SESSION_TOKEN)
+output=$($PYTHON_EXEC aws-auth-header.py $ACCESS_KEY_ID $SECRET_ACCESS_KEY $SESSION_TOKEN $AWS_STS_REGION)
 auth_header=$(echo $output | jq -r ".Authorization")
 date=$(echo $output | jq -r ".Date")
+CERT_PUBKEY=$(cat ${SYSTEM_SSH_DIR}/ssh_host_rsa_key.pub | base64 | tr -d \\n)
 
-echo "{\"auth\": {
-        \"amzDate\": \"${date}\", 
-        \"authorizationHeader\": \"${auth_header}\", 
-        \"sessionToken\": \"${SESSION_TOKEN}\"
+INNER_JSON=$(jq -n \
+  --arg amzDate "$date" \
+  --arg authHeader "$auth_header" \
+  --arg sessionToken "$SESSION_TOKEN" \
+  --arg certPubkey "$CERT_PUBKEY" \
+  --arg action "$CA_ACTION" \
+  --arg awsRegion "$AWS_STS_REGION" \
+  '{
+    auth: {
+      amzDate: $amzDate,
+      authorizationHeader: $authHeader,
+      sessionToken: $sessionToken
     },
-    \"sslAttrs\": {
-        \"validityPeriod\": \"${SSL_ATTRS_VALIDITY}\",
-        \"clientPublicKeyPem\": \"${SSL_CLIENT_PUBKEY_PEM}\"
-    },
-    \"sshAttrs\": {
-        \"validity\": \"${SSH_ATTRS_VALIDITY}\",
-        \"sshHostRSAKey\": \"${SSH_HOST_RSA_PUBKEY}\",
-        \"sshClientRSAKey\": \"${SSH_CLIENT_RSA_PUBKEY}\"
-    },
-    \"action\": \"${CA_ACTION}\"
-    }" > event.json
+    certPubkey: $certPubkey,
+    action: $action,
+    awsSTSRegion: $awsRegion
+  }' | jq -c)
 
+# JSON with body as stringified JSON
+final_json=$(jq -n --arg body "$INNER_JSON" '{body: $body}')
+echo "$final_json" > event.json
 
-aws lambda invoke --function-name ${CA_LAMBDA_FUNCTION_NAME} --cli-binary-format raw-in-base64-out --payload file://event.json response.json --region $LAMBDA_REGION
-response_body=$(cat response.json | jq -r ".body" | tr -d '"' | sed 's/\\r\\n/ \
-/g')
+aws lambda invoke \
+    --function-name ${CA_LAMBDA_FUNCTION_NAME} \
+    --cli-binary-format raw-in-base64-out \
+    --payload file://event.json \
+    response.json \
+    --region $LAMBDA_REGION \
+    --profile $AWS_PROFILE
+
+response_body=$(cat response.json | jq -r ".body" | tr -d '"' | sed 's/\\r\\n/\\n/g')
 
 echo ${response_body}
 
